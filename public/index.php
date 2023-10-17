194ecf73e854e6a66bc55d0209b86d5c2b6db55b
HugoGarrigues <113009447+HugoGarrigues@users.noreply.github.com>
2023-10-17 10:34:21 +0200

    Add Html button

--- public/index.php
+++ public/index.php
@@ -13,10 +13,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     if ($tabTrie !== null) {
         echo ($tab)."\n";
-        echo json_encode($tabTrie);
-
+        echo json_encode($tabTrie)."\n";
     }
 }
+
+$tri1 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=quickSort&tab=[5,3,1,4,2]';
+$tri2 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=bubbleSort&tab=[5,3,1,4,2]';
+
+echo '<a href="'.$tri1.'"><button>QuickSort</button></a>'."\n";
+echo '<a href="'.$tri2.'"><button>BubbleSort</button></a>'."\n";
 
 #http://localhost/Exercice-Api-Tri/public/index.php?tri=quickSort&tab=[5,3,1,4,2]
 #http://localhost/Exercice-Api-Tri/public/index.php?tri=bubbleSort&tab=[5,3,1,4,2]
