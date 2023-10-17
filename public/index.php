0c872362ed90ae0dcf85e1933be8cf26f4c0da87
HugoGarrigues <113009447+HugoGarrigues@users.noreply.github.com>
2023-10-17 11:05:50 +0200

    Add randomTab

--- public/index.php
+++ public/index.php
@@ -17,8 +17,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
 }
 
-$tri1 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=quickSort&tab=[5,3,1,4,2]';
-$tri2 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=bubbleSort&tab=[5,3,1,4,2]';
+$randomTab = [];
+for ($i=0;$i<10;$i++) {
+    $n = rand(1, 9);
+    $randomTab[] = $n;
+}
+
+$randomTabJSON = json_encode($randomTab);
+
+
+$tri1 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=quickSort&tab='.$randomTabJSON;
+$tri2 = 'http://localhost/Exercice-Api-Tri/public/index.php?tri=bubbleSort&tab='.$randomTabJSON;
 
 echo '<a href="'.$tri1.'"><button>QuickSort</button></a>'."\n";
 echo '<a href="'.$tri2.'"><button>BubbleSort</button></a>'."\n";
